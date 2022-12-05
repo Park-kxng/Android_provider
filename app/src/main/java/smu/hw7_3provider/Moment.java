a2f276f5acd5af6df0a480d2722036807cabfd55
Yujin Jeong <yujin45@sookmyung.ac.kr>
2022-12-05 23:46:20 +0900

    소스작성

--- app/src/main/java/smu/hw7_3provider/Moment.java
+++ app/src/main/java/smu/hw7_3provider/Moment.java
@@ -2,4 +2,14 @@ package smu.hw7_3provider;
 public class Moment {
     // 뭔가 계절마다의 순간이 찍힌 사진을 객체로 만들려고 하는데 안 쓸 수 있음
     // 필요 없을 수도 있음
+    // 일단 이미지 경로 저장하는 부분
+    String path;
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
 }
--- app/src/main/java/smu/hw7_3provider/MomentsOfTheSeason.java
+++ app/src/main/java/smu/hw7_3provider/MomentsOfTheSeason.java
@@ -1,19 +1,43 @@
 package smu.hw7_3provider;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 // 선택된 계절에 찍은 이미지들 보여줄 부분
 
 public class MomentsOfTheSeason extends AppCompatActivity {
     RecyclerView recyclerView;
+    public static ArrayList<Moment> dataList;
 
+    int PERMISSION_ALL = 1;
+    private String TAG = "OurTAG";
+    String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.INTERNET
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
         setContentView(R.layout.activity_moments_of_the_season);
 
         //Intent로 어떤 계절을 볼건지 가져옴
@@ -28,7 +52,18 @@ public class MomentsOfTheSeason extends AppCompatActivity {
         // 앞에서 어떤 버튼을 눌렀는지에 따라서 선택된 계절을 보여줌
         if (whatSeason.equals("spring")){
             // 봄을 선택했을 때
+            if (!hasPermissions(this, PERMISSIONS)) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+            }
 
+            //백스레드 실행시켜 이미지 로드
+            BackThread backThread = new BackThread(handler);
+            backThread.start();
+            try {
+                backThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }else if(whatSeason.equals("summer")){
 
         }else if(whatSeason.equals("fall")){
@@ -39,4 +74,115 @@ public class MomentsOfTheSeason extends AppCompatActivity {
 
     }
     ///아놔
+    /*
+     MediaStore.Images.Media.DATE_ADDED,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.MIME_TYPE,
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media._ID
+    * */
+
+
+
+
+
+    private void getPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET
+        }, 10);
+    }
+
+    public boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+            if (msg.what == 11) {
+                System.out.println("Thread 전달");
+                //메인스레드에서 데이터 크기 확인. 전달 성공했는지 확인하기 위함.
+                System.out.println("Size of photo data list in MainThread: " + dataList.size());
+            }
+            return true;
+        }
+    });
+
+
+
+    // 저장소 권한 확인 함수
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 0) {
+            for (int grantResult : grantResults) {
+                if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(getApplicationContext(), "앱 권한 설정이 필요합니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            // 잘 되고 있나 찍어보기
+            Log.d(TAG, "Permission: " + permissions[0] + "was" + grantResults[0]);
+        } else {
+            Log.d(TAG, "Permission denied");
+        }
+    }
+
+    //갤러리 동기화
+    public ArrayList<Moment> ImageReady() {
+        ArrayList<Moment> pDataList = new ArrayList<>();
+
+        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = new String[]{
+                MediaStore.Images.Media.DATE_ADDED,
+                MediaStore.Images.Media.DISPLAY_NAME,
+                MediaStore.Images.Media.MIME_TYPE,
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media._ID
+        };
+        String selection = MediaStore.Images.Media.MIME_TYPE + "='image/jpeg'";
+        Cursor cursor = getApplicationContext().getContentResolver().query(uri, projection, selection, null, MediaStore.Images.Media.DATE_ADDED);
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        while (cursor.moveToNext()) {
+            String absolutePath = cursor.getString(columnIndex);
+            Moment pData = new Moment();
+            pData.setPath(absolutePath);
+            pDataList.add(pData);
+
+        }
+        return pDataList;
+    }
+
+    //백스레드에서 갤러리 동기화 실행
+    class BackThread extends Thread {
+        private Handler handler;
+
+        BackThread(Handler handler) {
+            this.handler = handler;
+        }
+
+        ArrayList<Moment> pDataBackList = new ArrayList<>();
+
+        @Override
+        public void run() {
+            pDataBackList = ImageReady();
+
+            //백스레드에서 데이터의 크기 확인
+            System.out.println("Size of photo data list in BackThread: " + pDataBackList.size());
+
+            MomentsOfTheSeason.dataList = pDataBackList;
+            handler.sendEmptyMessage(11);
+        }
+    }
 }
